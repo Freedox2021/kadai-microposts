1390bcdf5a9b0efed9c6070e5d9c005cb8b547b3
Ubuntu <info.freedox@gmail.com>
2021-05-23 14:41:03 +0000

    0523v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
+ root to: 'toppages#index'
 
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ 
+ post   "favorites/:micropost_id/create", to: "favorites#create"
+ delete "favorites/:micropost_id/destroy", to: "favorites#destroy"
 
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create] do
-    member do
-      get :followings
-      get :followers
-    end
-  end
-
-  resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
+ get 'signup', to: 'users#new'
+ resources :users, only: [:index, :show, :create] do
+ member do
+ get :followings
+ get :followers
+ get :likes
+end
+    collection do
+      get :search
+     end
+end
+ resources :microposts, only: [:create, :destroy]
+ resources :relationships, only: [:create, :destroy]
+ resources :favorites, only: [:create, :destroy]
 end
